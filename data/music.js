b254f8d5df281e8c0b2765994b1304a9fb76642c
Jeremias Mai <jeremiasmaiasd@gmail.com>
2024-02-19 15:24:49 -0300

    first commit

---
+++ data/music.js
@@ -0,0 +1,164 @@
+export const music = [
+  {
+    name: "Los manseros santiagueños",
+    disc: "/image/Logodisco.jpeg",
+    music: "/music/eterno_Amor.mp3",
+  },
+  {
+    name: "Shakira - Hips Don't Lie",
+    disc: "/image/shakira.jpeg",
+    music: "/music/Shakira - Hips Dont Lie.mp3",
+  },
+  {
+    name: "Post Malone ft. 21 Savage - Rockstar",
+    disc: "/image/Post Malone ft. 21 Savage - rockstar.jpg",
+    music: "/music/Post Malone - rockstar.mp3",
+  },
+  {
+    name: "Coldplay - Yellow",
+    disc: "/image/Coldplay - Yellow.jpeg",
+    music: "/music/Coldplay - Yellow.mp3",
+  },
+  {
+    name: "Astrix - Deep Jungle Walk",
+    disc: "/image/Astrix - Deep Jungle Walk.jpg",
+    music: "/music/Astrix - Deep Jungle Walk.mp3",
+  },
+  {
+    name: "Eminem - Without Me",
+    disc: "/image/Eminem - Without Me.jpg",
+    music: "/music/Eminem - Without Me.mp3",
+  },
+  {
+    name: "Dua Lipa - Love Again",
+    disc: "/image/Dua Lipa - Love Again.jpeg",
+    music: "/music/Dua Lipa - Love Again.mp3",
+  },
+
+  {
+    name: "Stromae - carmen",
+    disc: "/image/Stromae - carmen.jpg",
+    music: "/music/Stromae  carmen.mp3",
+  },
+
+  {
+    name: "Earth Angel · Marvin Berry · The Starlighters",
+    disc: "/image/Earth Angel · Marvin Berry · The Starlighters.jpg",
+    music: "/music/Earth Angel.mp3",
+  },
+
+  {
+    name: "Bright Eyes-Art Garfunkel",
+    disc: "/image/Bright Eyes-Art Garfunkel.jpg",
+    music: "/music/Bright Eyes  -  Art Garfunkel.mp3",
+  },
+
+  {
+    name: "Somewhere over the Rainbow - Israel IZ Kamakawiwo ole",
+    disc: "/image/Somewhere over the Rainbow - Israel Kamakawiwo.jpeg",
+    music: "/music/Somewhere over the Rainbow.mp3",
+  },
+
+  {
+    name: "Sie7e - Tengo tu love",
+    disc: "/image/Sie7e - Tengo tu love.jpg",
+    music: "/music/Sie7e - Tengo Tu Love.mp3",
+  },
+
+  {
+    name: "Calle 13 - La Vuelta al Mundo",
+    disc: "/image/Calle 13 - La Vuelta al Mundo.jpeg",
+    music: "/music/La Vuelta al Mundo.mp3",
+  },
+
+  {
+    name: "Tan Bionica - Ciudad Mágica",
+    disc: "/image/Tan Bionica - Ciudad Mágica.jpeg",
+    music: "/music/Ciudad Mágica.mp3",
+  },
+  {
+    name: "Avicii - Hey Brother",
+    disc: "/image/Avicii - Hey Brother.jpeg",
+    music: "/music/Avicii - Hey Brother.mp3",
+  },
+  {
+    name: "Y2K, bbno$ - Lalala",
+    disc: "/image/Y2K, bbno$ - Lalala.jpg",
+    music: "/music/Y2K bbno  Lalala.mp3",
+  },
+  {
+    name: "Pickle - On The Drums",
+    disc: "/image/Pickle - On The Drums.jpg",
+    music: "/music/Pickle - On The Drums.mp3",
+  },
+  {
+    name: "Miranda!, Chano - Mentía",
+    disc: "/image/Miranda!, Chano - Mentía.jpeg",
+    music: "/music/Miranda Chano  Mentía.mp3",
+  },
+  {
+    name: "Lost Frequencies ft. James Blunt - Melody",
+    disc: "/image/Lost Frequencies ft. James Blunt - Melody.jpg",
+    music: "/music/Lost Frequencies James Blunt Melody.mp3",
+  },
+  {
+    name: "Ed Sheeran - Bad Habits",
+    disc: "/image/Ed Sheeran - Bad Habits.jpeg",
+    music: "/music/Ed Sheeran  Bad Habits.mp3",
+  },
+  {
+    name: "Ke personajes ft La contra Otro día más",
+    disc: "/image/Ke personajes ft La contra Otro día más.jpeg",
+    music: "/music/Ke personajes Otro día más.mp3",
+  },
+  {
+    name: "La Beriso y Cacho Castaña - Cacho de Buenos Aires",
+    disc: "/image/La Beriso y Cacho Castaña - Cacho de Buenos Aires.jpg",
+    music: "/music/La Beriso y Cacho Castaña Cacho de Buenos Aires.mp3",
+  },
+  {
+    name: "Imagine Dragons - Demons",
+    disc: "/image/Imagine Dragons - Demons.jpeg",
+    music: "/music/Imagine Dragons  Demons.mp3",
+  },
+  {
+    name: "Mike Posner - I Took A Pill In Ibiza",
+    disc: "/image/Mike Posner - I Took A Pill In Ibiza.jpg",
+    music: "/music/Mike Posner  I Took A Pill In Ibiza.mp3",
+  },
+  {
+    name: "Sia - Cheap Thrills",
+    disc: "/image/Sia - Cheap Thrills.jpeg",
+    music: "/music/Sia  Cheap Thrills.mp3",
+  },
+  {
+    name: "David Guetta - Lovers On The Sun",
+    disc: "/image/David Guetta - Lovers On The Sun.jpeg",
+    music: "/music/David Guetta Lovers On The Sun.mp3",
+  },
+  {
+    name: "Taylor Swift - You Need To Calm Down",
+    disc: "/image/Taylor Swift - You Need To Calm Down.jpeg",
+    music: "/music/You Need To Calm Down Taylor Swift.mp3",
+  },
+  {
+    name: "Deorro - Bailar feat. Elvis Crespo",
+    disc: "/image/Deorro - Bailar feat. Elvis Crespo.jpeg",
+    music: "/music/Deorro  Bailar.mp3",
+  },
+  {
+    name: "R3HAB & DEORRO - Flashlight",
+    disc: "/image/R3HAB & DEORRO - Flashlight.jpg",
+    music: "/music/R3HAB & DEORRO Flashlight.mp3",
+  },
+  {
+    name: "R3hab & Trevor Guthrie - SoundWave",
+    disc: "/image/R3hab & Trevor Guthrie - SoundWave.jpg",
+    music: "/music/R3hab & Trevor Guthrie  SoundWave.mp3",
+  },
+  {
+    name: "Hardwell feat. Chris Jones - Young Again",
+    disc: "/image/Hardwell feat. Chris Jones - Young Again.jpeg",
+    music: "/music/Hardwell Young Again.mp3",
+  },
+];
---
+++ js/app.js
@@ -0,0 +1,130 @@
+import { music } from "../data/music.js";
+const msjalert = document.querySelector(".container_msj_alert");
+const containerall = document.querySelector(".container");
+const container = document.querySelector(".list-music__music");
+const containermusic = document.querySelector(".containerMusic__cards");
+const pop_ups = document.querySelector(".pop-ups");
+const templatemusic = document.querySelector(".templatecards");
+const template = document.querySelector(".template");
+const arrow = document.querySelector(".arrow-list");
+const fragment = document.createDocumentFragment();
+let audio = "";
+let state = false;
+let statearrow = false;
+let music_list = [];
+music.forEach((item) => {
+  const clone = templatemusic.content.cloneNode(true);
+  clone.querySelector("article .card-title").textContent = item.name;
+  clone.querySelector("article .card-img ").src = item.disc;
+  clone.querySelector("button i").dataset.disc = item.disc;
+  clone.querySelector("button i").dataset.title = item.name;
+  clone.querySelector("button i").dataset.path = item.music;
+
+  fragment.appendChild(clone);
+});
+containermusic.appendChild(fragment);
+document.addEventListener("click", (e) => {
+  // quit alert card
+  if (e.target.matches(".alert__btn")) {
+    msjalert.style.display = "none";
+    containerall.style.filter = "none";
+  }
+  if (e.target.matches(".bi-plus-circle-fill")) {
+    addmusic(e);
+  }
+  // burger list
+  if (e.target.matches("#burgerlist_mobile")) {
+    if (state) {
+      pop_ups.classList.add("showburger");
+      pop_ups.classList.remove("hideburger");
+      state = false;
+    } else {
+      pop_ups.classList.add("hideburger");
+      pop_ups.classList.remove("showburger");
+      state = true;
+    }
+  }
+  // flip arrow
+  if (e.target.matches(".bi-caret-up-fill")) {
+    if (statearrow) {
+      arrow.style.transform = "rotate(0deg)";
+      statearrow = false;
+    } else {
+      arrow.style.transform = "rotate(180deg)";
+      statearrow = true;
+    }
+  }
+  if (e.target.matches(".bi-play-fill")) {
+    btnplay(e);
+  }
+  if (e.target.matches(".bi-pause-fill")) {
+    btnpause(e);
+  }
+  if (e.target.matches(".bi-trash")) {
+    music_list.filter((item) => {
+      return item.id !== e.target.dataset.id;
+    });
+  }
+});
+
+// search music
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") e.target.value = "";
+  if (e.target.matches(".cards")) {
+    containermusic.forEach((item) => {
+      item
+        .querySelectorAll("article .card-title")
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase())
+        ? item.querySelectorAll("article").classList.remove("filter")
+        : item.querySelectorAll("article").classList.add("filter");
+    });
+  }
+});
+
+const addmusic = (e) => {
+  if (music_list.length === 8) {
+    return console.log("ya esta lleno");
+  } else {
+    const music = {
+      title: e.target.dataset.title,
+      id: e.target.dataset.title,
+      disc: e.target.dataset.disc,
+      path: e.target.dataset.path,
+    };
+    const position = music_list.findIndex((item) => {
+      return item.title === music.title;
+    });
+    if (position === -1) {
+      music_list.push(music);
+    }
+  }
+  showmusic(e);
+};
+
+const showmusic = (e) => {
+  container.textContent = "";
+  music_list.forEach((item) => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector("div .list-music-img").src = item.disc;
+    clone.querySelector("div .list-music-title").textContent = item.title;
+    clone.querySelector("div .delete").dataset.id = item.id;
+    clone.querySelector("div .controlplay").dataset.id = item.id;
+    clone.querySelector("div .bi-play-fill").dataset.id = item.id;
+    fragment.appendChild(clone);
+  });
+  container.appendChild(fragment);
+};
+const btnplay = (e) => {
+  music_list.forEach((item) => {
+    if (e.target.dataset.id === item.id) {
+      audio = new Audio(item.path);
+      audio.play();
+    }
+  });
+};
+const btnpause = (e) => {
+  if (audio.play()) {
+    audio.pause();
+  }
+};
